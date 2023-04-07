6a0adefce9ff332beec176c0801f823709cb905a
zk20221025 <1036767219@qq.com>
2023-04-07 14:30:42 +0800

    50

--- 50_ERC20/50_ERC20.sol
+++ 50_ERC20/50_ERC20.sol
@@ -82,14 +82,14 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contr
 contract MyToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         // Mint 100 tokens to msg.sender
-        // Similar to how
+        // 类似于
         // 1 dollar = 100 cents
         // 1 token = 1 * (10 ** decimals)
         _mint(msg.sender, 100 * 10 ** uint(decimals()));
     }
 }
 
-transferFrom(address sender, address recipient, uint256 amount)
+// transferFrom(address sender, address recipient, uint256 amount)
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
